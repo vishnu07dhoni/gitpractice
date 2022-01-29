efe05ff874a5ce907fe4b8ea031ec2d8f84c0111
“vishnu07dhoni” <vishnusiva2917@gmail.com>
2022-01-29 09:01:41 +0000

    gitpractice - Vishnu - 2nd commit

--- LoanBookingSessionBean.java
+++ LoanBookingSessionBean.java
@@ -3,8 +3,7 @@ import java.lang.Math;
 import java.io.printer; 
 import java.io.collection; 
 import java.io.scanner;
-import java.io.outputprinter;
-import java.io.inputprinter;
+
 public class SpiralPatternExample2
 {  
 //function to print the spiral pattern  
